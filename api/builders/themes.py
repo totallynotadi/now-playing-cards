c24401f2eaedc4a01df7847788e194800fa369f2
addyett <g.aditya2048@gmail.com>
2022-07-17 15:31:24 +0530

    chore: change background color for small card

--- api/builders/themes.py
+++ api/builders/themes.py
@@ -1,7 +1,8 @@
 text_theme_dark = {'bar_color': 'rgba(147, 147, 147, 1)', 'text_color': 'rgba(96, 95, 95, 1)'}
 text_theme_light = {'bar_color': 'rgba(188, 188, 188, 1)', 'text_color': 'white'}
 
-background_dark = 'rgba(40, 64, 102, 1)'
+# background_dark = 'rgba(40, 64, 102, 1)'
+background_dark = 'rgba(53, 71, 99, 1)'
 background_light = 'rgba(229, 229, 229, 1)'
 
 def get_theme(image):
--- api/builders/utils.py
+++ api/builders/utils.py
@@ -1,9 +1,14 @@
 from PIL import ImageFont
 
+import os
+
 
 SIZES = {'title': 'api/builders/fonts/inter-500.ttf', 'artist': 'api/builders/fonts/inter-600.ttf', 'subtitle': 'api/builders/fonts/inter-500.ttf'}
 
 def get_text_len(text, font_size, font_name):
+    print(f":::current director - {os.getcwd()}")
+    print(f"::::directory listing - {os.listdir()}")
+    print(f"::::{os.listdir('api/builders/')}")
     font = ImageFont.truetype(SIZES[font_name], font_size)
     size = font.getsize(text)
     return size[0]
